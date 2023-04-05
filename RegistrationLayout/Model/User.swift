3e81a69c0950774ec93a77684029034b308c2e3b
Ilnur Fattakhov <nurich1995@mail.ru>
2023-04-06 00:33:18 +0300

    finish all viewControllers and started add Model

--- RegistrationLayout/Model/User.swift
+++ RegistrationLayout/Model/User.swift
@@ -7,3 +7,15 @@
 
 import Foundation
 
+struct User {
+    let login = "Alexey"
+    let password = "Password"
+    let person: Person
+}
+
+struct Person {
+    let nameAndSurname = "Ilnur Fattakhov"
+    let company = "KAMSS"
+    let position = "Specialist of certification"
+    let department = "Development"
+}
---
+++ RegistrationLayout/ViewControllers/BioViewController.swift
@@ -0,0 +1,31 @@
+//
+//  BioViewController.swift
+//  RegistrationLayout
+//
+//  Created by Ilnur on 05.04.2023.
+//
+
+import UIKit
+
+class BioViewController: UIViewController {
+
+    private let primaryColor = UIColor(
+        red: 210/255,
+        green: 109/255,
+        blue: 128/255,
+        alpha: 1
+    )
+    
+    private let secondaryColor = UIColor(
+        red: 107/255,
+        green: 148/255,
+        blue: 230/255,
+        alpha: 1
+    )
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addVerticalGradientLayer(topColor: primaryColor, bottomColor: secondaryColor)
+    }
+}
+
