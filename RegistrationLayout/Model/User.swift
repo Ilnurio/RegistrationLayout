2c2e79901e3cc104a0dd12d7f2e342fe25060371
Ilnur Fattakhov <nurich1995@mail.ru>
2023-04-05 23:35:04 +0300

    add viewControllers

---
+++ RegistrationLayout/Model/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  RegistrationLayout
+//
+//  Created by Ilnur on 05.04.2023.
+//
+
+import Foundation
+
---
+++ RegistrationLayout/ViewControllers/TabBarController.swift
@@ -0,0 +1,15 @@
+//
+//  TabBarController.swift
+//  RegistrationLayout
+//
+//  Created by Ilnur on 05.04.2023.
+//
+
+import UIKit
+
+final class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
